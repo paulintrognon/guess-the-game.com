2aed9cfb7614dc00cd8db890ce076b3828ce3bc4
Paulin Trognon <paulin.trognon@gmail.com>
2018-07-06 20:55:03 +0200

    add seed/sql

---
+++ server/db/migrations/seed.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Ven 06 Juillet 2018 à 20:54
+-- Version du serveur :  10.1.26-MariaDB-0+deb9u1
+-- Version de PHP :  7.0.30-0+deb9u1
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `guessthegame`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ScreenshotFounds`
+--
+
+DROP TABLE IF EXISTS `ScreenshotFounds`;
+CREATE TABLE `ScreenshotFounds` (
+  `id` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `UserId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ScreenshotNames`
+--
+
+DROP TABLE IF EXISTS `ScreenshotNames`;
+CREATE TABLE `ScreenshotNames` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `ScreenshotId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Screenshots`
+--
+
+DROP TABLE IF EXISTS `Screenshots`;
+CREATE TABLE `Screenshots` (
+  `id` int(11) NOT NULL,
+  `gameCanonicalName` varchar(255) NOT NULL,
+  `difficulty` int(11) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `UserId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `screenshotsFound` int(11) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `deletedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `ScreenshotFounds`
+--
+ALTER TABLE `ScreenshotFounds`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `UserId` (`UserId`);
+
+--
+-- Index pour la table `ScreenshotNames`
+--
+ALTER TABLE `ScreenshotNames`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ScreenshotId` (`ScreenshotId`);
+
+--
+-- Index pour la table `Screenshots`
+--
+ALTER TABLE `Screenshots`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `UserId` (`UserId`);
+
+--
+-- Index pour la table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `ScreenshotFounds`
+--
+ALTER TABLE `ScreenshotFounds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `ScreenshotNames`
+--
+ALTER TABLE `ScreenshotNames`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Screenshots`
+--
+ALTER TABLE `Screenshots`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `ScreenshotFounds`
+--
+ALTER TABLE `ScreenshotFounds`
+  ADD CONSTRAINT `ScreenshotFounds_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `ScreenshotNames`
+--
+ALTER TABLE `ScreenshotNames`
+  ADD CONSTRAINT `ScreenshotNames_ibfk_1` FOREIGN KEY (`ScreenshotId`) REFERENCES `Screenshots` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Screenshots`
+--
+ALTER TABLE `Screenshots`
+  ADD CONSTRAINT `Screenshots_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+SET FOREIGN_KEY_CHECKS=1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
