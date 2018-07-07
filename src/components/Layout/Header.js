0a35c9f700effeff6dbde48433840727a42f9757
Paulin Trognon <paulin.trognon@gmail.com>
2018-07-07 14:10:37 +0200

    fix eslint warning

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,11 +5,7 @@ import './header.css';
 export default function Layout() {
   return (
     <header className="Header">
-      <nav
-        className="navbar has-shadow is-spaced"
-        role="navigation"
-        aria-label="main navigation"
-      >
+      <nav className="navbar has-shadow is-spaced">
         <div className="column" />
         <div className="column">
           <h1 className="Header__title title">Guess the game!</h1>
