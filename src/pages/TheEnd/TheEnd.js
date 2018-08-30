c2b1d71ae0806bb59c9d6073400f414467d71833
Paulin Trognon <paulin.trognon@gmail.com>
2018-08-30 20:31:20 +0200

    design the end page

--- src/pages/TheEnd/TheEnd.js
+++ src/pages/TheEnd/TheEnd.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import './TheEnd.css';
 
 export default () => (
-  <section className="section">
-    <div className="container">
-      <p>You have resolved all screenshots... Congratz.</p>
-      <p>
-        <img
-          src="https://media3.giphy.com/media/L0HAj70HJ3A6A/giphy.gif"
-          alt="Bravo"
-        />
-      </p>
-    </div>
+  <section className="TheEndPage">
+    <p>You have resolved all screenshots... Congratz.</p>
+    <p>
+      <img
+        className="TheEndPage_gif"
+        src="https://media3.giphy.com/media/L0HAj70HJ3A6A/giphy.gif"
+        alt="Bravo"
+      />
+    </p>
   </section>
 );
