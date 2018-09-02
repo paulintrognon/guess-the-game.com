7c575e436a6eed7d0fdd6681d21972194a0b44e9
Paulin Trognon <paulin.trognon@gmail.com>
2018-09-02 21:30:15 +0200

    fix scores

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -40,7 +40,7 @@ class Homepage extends React.Component {
         {scores.map((score, i) => (
           <div className="RankingPage_ranking_row">
             <div className="RankingPage_ranking_col -name">
-              <span className="RankingPage_ranking_nb">#{i + 6}</span>{' '}
+              <span className="RankingPage_ranking_nb">{iToRank(i)}</span>{' '}
               <span className="RankingPage_ranking_username">
                 {score.username}
               </span>
@@ -62,3 +62,21 @@ class Homepage extends React.Component {
   }
 }
 export default connect(mapStoreToProps)(Homepage);
+
+function iToRank(i) {
+  const rank = i + 1;
+  return rank + rankToText(rank);
+}
+
+function rankToText(rank) {
+  if (rank % 10 === 1 && rank !== 11) {
+    return 'st';
+  }
+  if (rank % 10 === 2 && rank !== 12) {
+    return 'nd';
+  }
+  if (rank % 10 === 3 && rank !== 13) {
+    return 'rd';
+  }
+  return 'th';
+}
