c2a719f879b95d9018d071800937295845fea831
Paulin Trognon <paulin.trognon@gmail.com>
2018-11-10 04:02:42 +0100

    integrate my account page

--- src/pages/User/Account/Account.jsx
+++ src/pages/User/Account/Account.jsx
@@ -4,18 +4,43 @@ import Helmet from 'react-helmet';
 import './Account.css';
 import BarTitle from '../../../components/BarTitle/BarTitle';
 
-function AccountPage() {
-  return (
-    <section className="section">
-      <Helmet title="My account" />
-      <div className="container">
-        <div className="AccountPage">
-          <BarTitle hideOnSmall>
-            <h2>My Account</h2>
-          </BarTitle>
+const AccountPage = () => (
+  <section className="section">
+    <Helmet title="My account" />
+    <div className="container">
+      <div className="AccountPage">
+        <BarTitle hideOnSmall>
+          <h2>My Account</h2>
+        </BarTitle>
+        <div className="AccountPage_content">
+          <div className="AccountPage_data">
+            <div className="AccountPage_data_line">
+              <p className="AccountPage_data_line_left">Username</p>
+              <p className="AccountPage_data_line_right">Le Tod</p>
+            </div>
+            <div className="AccountPage_data_line">
+              <p className="AccountPage_data_line_left">Email</p>
+              <p className="AccountPage_data_line_right">tod5@hotmail.com</p>
+            </div>
+            <div className="AccountPage_data_line">
+              <p className="AccountPage_data_line_left">Password</p>
+              <p className="AccountPage_data_line_right">
+                <a href="/forgot-password">reset it!</a>
+              </p>
+            </div>
+            <hr />
+            <div className="AccountPage_data_line">
+              <p className="AccountPage_data_line_left">Screenshots found</p>
+              <p className="AccountPage_data_line_right">27</p>
+            </div>
+            <div className="AccountPage_data_line">
+              <p className="AccountPage_data_line_left">Screenshots posted</p>
+              <p className="AccountPage_data_line_right">12</p>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
-  );
-}
+    </div>
+  </section>
+);
 export default AccountPage;
