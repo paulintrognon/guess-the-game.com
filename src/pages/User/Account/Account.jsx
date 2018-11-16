8e0bdc4971bdca0a6ab02dad7c553ad19b7d8f72
Paulin Trognon <paulin.trognon@gmail.com>
2018-11-16 07:24:01 +0100

    fix some styles

--- src/pages/User/Account/Account.jsx
+++ src/pages/User/Account/Account.jsx
@@ -2,76 +2,74 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import userActions from '../../../actions/userActions';
 import './Account.css';
 import BarTitle from '../../../components/BarTitle/BarTitle';
+import Loading from '../../../components/Loading/Loading';
+import Button from '../../../components/Form/Button/Button';
+import loginActions from '../../../actions/loginActions';
 
 function mapStoreToProps(store) {
-  return {
-    userData: store.user.userData,
-  };
+  return store.user.userData || {};
 }
-class AccountPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props.dispatch(userActions.loadUserData());
-  }
-
-  render() {
-    const { userData } = this.props;
-    return (
-      <section className="section">
-        <Helmet title="My account" />
-        <div className="AccountPage">
-          <BarTitle hideOnSmall>
-            <h2>My Account</h2>
-          </BarTitle>
-          <div className="AccountPage_content">
-            {userData ? (
-              <div className="AccountPage_data">
-                <div className="AccountPage_data_line">
-                  <p className="AccountPage_data_line_left">Username</p>
-                  <p className="AccountPage_data_line_right">
-                    {userData.username}
-                  </p>
-                </div>
-                <div className="AccountPage_data_line">
-                  <p className="AccountPage_data_line_left">Email</p>
-                  <p className="AccountPage_data_line_right">
-                    {userData.email}
-                  </p>
-                </div>
-                <div className="AccountPage_data_line">
-                  <p className="AccountPage_data_line_left">Password</p>
-                  <p className="AccountPage_data_line_right">
-                    <Link to="/forgot-password">reset it!</Link>
-                  </p>
-                </div>
-                <hr />
-                <div className="AccountPage_data_line">
-                  <p className="AccountPage_data_line_left">
-                    Screenshots found
-                  </p>
-                  <p className="AccountPage_data_line_right">
-                    {userData.screenshotsFound}
-                  </p>
-                </div>
-                <div className="AccountPage_data_line">
-                  <p className="AccountPage_data_line_left">
-                    Screenshots posted
-                  </p>
-                  <p className="AccountPage_data_line_right">
-                    {userData.screenshotsAdded}
-                  </p>
-                </div>
-              </div>
-            ) : (
-              <p>Chargement...</p>
-            )}
+const AccountPage = ({
+  username,
+  email,
+  screenshotsFound,
+  screenshotsAdded,
+  dispatch,
+}) => (
+  <section className="section">
+    <Helmet title="My account" />
+    <div className="AccountPage">
+      <BarTitle hideOnSmall>
+        <h2>My Account</h2>
+      </BarTitle>
+      <div className="AccountPage_content">
+        <div className="AccountPage_data">
+          <div className="AccountPage_data_line">
+            <p className="AccountPage_data_line_left">Username</p>
+            <p className="AccountPage_data_line_right">{username}</p>
+          </div>
+          <div className="AccountPage_data_line">
+            <p className="AccountPage_data_line_left">Email</p>
+            <p className="AccountPage_data_line_right">{email}</p>
+          </div>
+          <div className="AccountPage_data_line">
+            <p className="AccountPage_data_line_left">Password</p>
+            <p className="AccountPage_data_line_right">
+              <Link to="/forgot-password">reset it!</Link>
+            </p>
+          </div>
+          <hr />
+          <div className="AccountPage_data_line">
+            <p className="AccountPage_data_line_left">Screenshots solved</p>
+            <p className="AccountPage_data_line_right">
+              {screenshotsFound || (
+                <Loading className="AccountPage_data_line_loading" />
+              )}
+            </p>
+          </div>
+          <div className="AccountPage_data_line">
+            <p className="AccountPage_data_line_left">Screenshots posted</p>
+            <p className="AccountPage_data_line_right">
+              {screenshotsAdded || (
+                <Loading className="AccountPage_data_line_loading" />
+              )}
+            </p>
+          </div>
+          <hr />
+          <div className="AccountPage_data_line -centered">
+            <Button
+              color="dark"
+              onClick={() => dispatch(loginActions.logout())}
+              className="AccountPage_data_line_logOutButton"
+            >
+              Log out
+            </Button>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </div>
+  </section>
+);
 export default connect(mapStoreToProps)(AccountPage);
