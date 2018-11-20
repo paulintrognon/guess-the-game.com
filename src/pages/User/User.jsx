83d1a7a4837c0a9599d5cdceaf55272103944a0d
Paulin Trognon <paulin.trognon@gmail.com>
2018-11-20 08:49:03 +0100

    fix user space access while not logged in. Closes #19

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router';
 import './User.css';
 import Menu from './Menu/Menu';
 import AccountPage from './Account/Account';
@@ -7,15 +8,25 @@ import ModerationPage from './Moderation/Moderation';
 import SolvedScreenshotsPage from './SolvedScreenshots/SolvedScreenshots';
 import AddedScreenshotsPage from './AddedScreenshots/AddedScreenshots';
 
-const UserPages = ({ match }) => (
-  <div className="UserPages">
-    <Menu />
-    <div className="UserPages_container">
-      <Route path={`${match.url}/account`} component={AccountPage} />
-      <Route path={`${match.url}/moderation`} component={ModerationPage} />
-      <Route path={`${match.url}/solved`} component={SolvedScreenshotsPage} />
-      <Route path={`${match.url}/added`} component={AddedScreenshotsPage} />
+function mapStoreToProps(store) {
+  return {
+    user: store.user,
+  };
+}
+const UserPages = ({ user, match }) => {
+  if (!user || !user.jwt) {
+    return <Redirect to="/login" />;
+  }
+  return (
+    <div className="UserPages">
+      <Menu />
+      <div className="UserPages_container">
+        <Route path={`${match.url}/account`} component={AccountPage} />
+        <Route path={`${match.url}/moderation`} component={ModerationPage} />
+        <Route path={`${match.url}/solved`} component={SolvedScreenshotsPage} />
+        <Route path={`${match.url}/added`} component={AddedScreenshotsPage} />
+      </div>
     </div>
-  </div>
-);
-export default UserPages;
+  );
+};
+export default connect(mapStoreToProps)(UserPages);
