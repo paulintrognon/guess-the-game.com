35916feb58b6941b05823ac793770dd934618b19
Paulin Trognon <paulin.trognon@gmail.com>
2019-07-07 20:34:06 +1200

    add loader when loading user solved & added screenshots

--- src/pages/User/AddedScreenshots/AddedScreenshots.jsx
+++ src/pages/User/AddedScreenshots/AddedScreenshots.jsx
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
 import BarTitle from '../../../components/BarTitle/BarTitle';
+import Loading from '../../../components/Loading/Loading';
 import ScreenshotsGrid from '../../../components/ScreenshotsGrid/ScreenshotsGrid';
 import userActions from '../../../actions/userActions';
 import './AddedScreenshots.css';
 
 function mapStoreToProps(store) {
   return {
-    addedScreenshots: store.user.addedScreenshots || [],
+    addedScreenshots: store.user.addedScreenshots,
   };
 }
 class AddedScreenshotsPage extends React.Component {
@@ -27,24 +28,33 @@ class AddedScreenshotsPage extends React.Component {
           <BarTitle onlyOnSmartphones>
             <h2>Screenshots Ajoutes</h2>
           </BarTitle>
-          <ScreenshotsGrid screenshots={addedScreenshots} canEditScreenshots>
-            <Link className="ScreenshotsGrid_item" to="/ajouter-un-screenshot">
-              <div className="AddedScreenshotsPage_item_add">
-                <svg
-                  className="AddedScreenshotsPage_item_add_icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
-                </svg>
-              </div>
-              <div className="AddedScreenshotsPage_item_legend">
-                <p>Ajouter un nouveau</p>
-              </div>
-            </Link>
-          </ScreenshotsGrid>
+          {addedScreenshots === null ? (
+            <p style={{ textAlign: 'center' }}>
+              <Loading />
+            </p>
+          ) : (
+            <ScreenshotsGrid screenshots={addedScreenshots} canEditScreenshots>
+              <Link
+                className="ScreenshotsGrid_item"
+                to="/ajouter-un-screenshot"
+              >
+                <div className="AddedScreenshotsPage_item_add">
+                  <svg
+                    className="AddedScreenshotsPage_item_add_icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
+                  </svg>
+                </div>
+                <div className="AddedScreenshotsPage_item_legend">
+                  <p>Ajouter un nouveau</p>
+                </div>
+              </Link>
+            </ScreenshotsGrid>
+          )}
         </div>
       </section>
     );
