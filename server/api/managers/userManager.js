0fbda257820d662fb9cd861ba303adce16f5700c
Paulin Trognon <paulin.trognon@gmail.com>
2019-07-25 15:02:17 +1200

    show screenshot approval status

--- server/api/managers/userManager.js
+++ server/api/managers/userManager.js
@@ -144,6 +144,7 @@ async function getAddedScreenshots(userId, extraFilters) {
     attributes: [
       'id',
       'gameCanonicalName',
+      'approvalStatus',
       'year',
       'createdAt',
       'ScreenshotImageId',
