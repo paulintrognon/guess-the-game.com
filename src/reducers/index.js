2472b87b15875385a3bba23cfc29cf440acff800
Paulin Trognon <paulin.trognon@gmail.com>
2019-08-27 12:59:31 +1200

    bump to react v16.9

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,17 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 
 import layoutReducer from './layoutReducer';
 import screenshotReducer from './screenshotReducer';
 import userReducer from './userReducer';
 import moderationReducer from './moderationReducer';
 
-export default combineReducers({
-  layout: layoutReducer,
-  screenshot: screenshotReducer,
-  user: userReducer,
-  moderation: moderationReducer,
-});
+const createRootReducer = history =>
+  combineReducers({
+    router: connectRouter(history),
+    layout: layoutReducer,
+    screenshot: screenshotReducer,
+    user: userReducer,
+    moderation: moderationReducer,
+  });
+export default createRootReducer;
---
+++ src/reducers/solvedScreenshotsReducer.js
@@ -0,0 +1,67 @@
+const initialState = {
+  isLoading: false,
+  hasMore: false,
+  solvedScreenshots: null,
+};
+
+export default function reducer(state = initialState, action) {
+  const newState = { ...state };
+  const { type, payload } = action;
+
+  if (type === 'USER_SOLVED-SCREENSHOTS_LOADED') {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+
+  if (type === 'USER_SOLVED-SCREENSHOTS_LOADED') {
+    return {
+      ...state,
+      solvedScreenshots: payload.map(screenshot => ({
+        ...screenshot,
+        createdAt: new Date(screenshot.createdAt),
+        solvedAt: new Date(screenshot.solvedAt),
+      })),
+    };
+  }
+
+  return newState;
+}
+
+function updateLastViewedRandomScreenshots({
+  lastViewedRandomScreenshots,
+  screenshotId,
+  reset,
+}) {
+  // Si on demande de reset, on recommance le tableau
+  if (reset) {
+    return [screenshotId];
+  }
+  // Si la screenshot est déjà, on ne fait rien
+  if (lastViewedRandomScreenshots.indexOf(screenshotId) > -1) {
+    return lastViewedRandomScreenshots;
+  }
+  // Si le nombre de screenshots dépasse la limite, on supprime la première avant de rajouter la nouvelle
+  if (lastViewedRandomScreenshots.length > 5000) {
+    return lastViewedRandomScreenshots.slice(1).concat([screenshotId]);
+  }
+  // Sinon, on ajoute la nouvelle screenshot à la liste des screenshots vues
+  return lastViewedRandomScreenshots.concat([screenshotId]);
+}
+
+function storeLastViewedRandomScreenshots(newValue) {
+  localStorage.setItem('lastViewedRandomScreenshots', newValue.join(','));
+}
+
+function retrieveStoredLastViewedRandomScreenshots() {
+  const lastViewedRandomScreenshots = localStorage.getItem(
+    'lastViewedRandomScreenshots'
+  );
+  if (!lastViewedRandomScreenshots) {
+    return [];
+  }
+  return lastViewedRandomScreenshots
+    .split(',')
+    .map(n => Number.parseInt(n, 10));
+}
